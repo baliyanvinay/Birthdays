43a70133def7204b8704fb523681117ebe7e75e3
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-09-09 13:15:43 +0530

    Search functionality added.

--- List_Birthdays/views.py
+++ List_Birthdays/views.py
@@ -3,12 +3,16 @@ from .models import Tab_Birthdays
 from django.http import HttpResponseRedirect
 
 # Create your views here.
-def index(request, search_name):
+def index(request):
+    search_name=request.GET.get('search_name').capitalize() 
     if search_name:
-        print(search_name)
-    model_data={
-        "Birthdays": Tab_Birthdays.objects.all()
-    }
+        model_data={
+        "Birthdays": Tab_Birthdays.objects.filter(name=search_name)
+        }
+    else:
+        model_data={
+            "Birthdays": Tab_Birthdays.objects.all()
+        }   
     return render(request, template_name='list_birthdays/index.html', context=model_data)
 
 def add_new(request):
@@ -19,9 +23,3 @@ def add_new(request):
         new_birthday=Tab_Birthdays(name=name, dob=dob, age=age)
         new_birthday.save()
     return render(request, template_name='list_birthdays/add_new.html')
-
-def search(request, search_name):
-    model_data={
-        "Birthdays": Tab_Birthdays.objects.filter(name=search_name)
-    }
-    return render(request, template_name='list_birthdays/index.html', context=model_data)
